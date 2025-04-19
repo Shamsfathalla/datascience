17725fd87f7a582dae7e0a361ff8fcf7ad2fca7c
Shamsfathalla <127939232+Shamsfathalla@users.noreply.github.com>
2025-04-19 16:38:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -418,7 +418,7 @@ area_type_map = {0: 'Rural', 1: 'Suburban', 2: 'Urban'}
 # Function to download and extract dataset
 @st.cache_data
 def load_dataset():
-    url = "https://github.com/Shamsfathalla/datascience/blob/c0597b32d9034e6732b2c488f6ed4f2c3515e1a6/datasets.zip"
+    url = "https://github.com/Shamsfathalla/datascience/raw/main/datasets.zip"
     try:
         response = requests.get(url)
         if response.status_code != 200:
